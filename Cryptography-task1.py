a6569a2e4d6536c712fdf7fdf0c160d603067532
Flaw <katherfahima@gmail.com>
2024-02-25 11:35:28 +0400

    Added comments, don't know if they're excessive and changed some stuff in task2try3

--- Cryptography-task1.py
+++ Cryptography-task1.py
@@ -1,127 +1,122 @@
 # Importing necessary libraries
-import math
-import string
-import sys
+import math  # For mathematical operations
+import string  # For string operations
+import sys  # For system-specific parameters and functions
 
-#The dictionary and list are both used in enc_convert() and dec_convert functions
-alpha_dict = {'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7, 'I' : 8, 'J' : 9, 'K' : 10, 'L' : 11, 'M' : 12, 'N' : 13, 'O' : 14, 'P' : 15, 'Q' : 16, 'R' : 17, 'S': 18, 'T' : 19, 'U': 20, 'V' : 21, 'W' : 22, 'X' : 23, 'Y' : 24, 'Z' : 25}
-alpha_list = list(string.ascii_uppercase)
-#This list is used in affine_encrypt() and affine_decrypt() functions
-spec_chara = ['~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/']
+# The dictionary and list are both used in enc_convert() and dec_convert functions
+letters = string.ascii_uppercase  # Get all uppercase letters
+alpha_dict = {letter: index for index, letter in enumerate(letters)}  # Dictionary mapping letters to their index
+alpha_list = list(letters)  # List of uppercase letters
 
-#This function prompts the user for what they want to do(encrypt? or decrypt?)
+# This function prompts the user for what they want to do (encrypt? or decrypt?)
 def enc_or_dec():
-    #if correct input is put in, the while loop terminates, otherwise it will loop again
+    # Loop until correct input is provided
     while True:
         print("Enter 1 for encryption, 2 for decryption, q to quit\n")
         try:
-            #s is the input the user puts in
-            s = input("What would you like to do: ")
-            #breaks while loop when correct input is given and raises Value error when incorrect value is given
-            if s == '1' or s == '2' or s == 'q':
-                break
+            s = input("What would you like to do: ")  # Get user input
+            if s == '1' or s == '2' or s == 'q':  # Check if input is valid
+                break  # Break the loop if input is valid
             else:
-                raise ValueError
+                raise ValueError  # Raise ValueError for incorrect input
         except ValueError:
             print("\n")
             print("Incorrect input, please input correct value\n")
-        #allows Ctrl+C to be used to interrupt the program
         except KeyboardInterrupt:
             print("\n")
-            sys.exit('Interrupted\n')
-    #returns s to main function
-    return s
+            sys.exit('Interrupted\n')  # Handle interrupt (Ctrl+C)
 
-#Gets keys from the user
+    return s  # Return user's choice
+
+# Gets keys from the user
 def get_keys():
     while True:
         try:
             print("\n")
             print("Enter Ctrl+C to quit")
             print("The first key must be a coprime of 26\n")
-            key1 = int(input("Enter first key: "))
-            key2 = int(input("Enter second key: "))
+            key1 = int(input("Enter first key: "))  # Get first key
+            key2 = int(input("Enter second key: "))  # Get second key
 
             while True:
-
+                # Ensure key1 is positive
                 if key1 < 0:
                     key1 += 26
                 else:
                     break
 
-            if math.gcd(key1, 26) == 1:
+            if math.gcd(key1, 26) == 1:  # Check if key1 is coprime with 26
                 break
             else:
                 print("not a coprime")
         except KeyboardInterrupt:
             print("\n")
-            sys.exit("Interrupted")
+            sys.exit("Interrupted")  # Handle interrupt (Ctrl+C)
         except ValueError:
-            print("Not a number")
-        
+            print("Not a number")  # Handle non-numeric input
 
-    return key1, key2
+    return key1, key2  # Return the keys
 
-#returns encrypted values to affine_encrypt
+# Returns encrypted values to affine_encrypt
 def enc_convert(letter, key1, key2):
-    m = (key1 * alpha_dict[letter] + key2) % 26
-    c = alpha_list[m]
-    return c
+    m = (key1 * alpha_dict[letter] + key2) % 26  # Encryption formula
+    c = alpha_list[m]  # Get encrypted letter
+    return c  # Return encrypted letter
 
-#Encrypts the given Cipher text by sending each letter to the enc_convert function
+# Encrypts the given Cipher text by sending each letter to the enc_convert function
 def affine_encrypt():
-    key1, key2 = get_keys() 
-    sent = input("What text do you want to encrypt?: ")
-    actual_sent = sent.upper()
-    changed_sent = ''
+    key1, key2 = get_keys()  # Get encryption keys
+    sent = input("What text do you want to encrypt?: ")  # Get text to encrypt
+    actual_sent = sent.upper()  # Convert text to uppercase
+    changed_sent = ''  # Initialize encrypted text
     for i in actual_sent:
         if i.isnumeric() == True:
             changed_sent = changed_sent
-        elif i in spec_chara:
+        elif i not in alpha_list and i != " ":
             changed_sent = changed_sent
         elif i == ' ':
             changed_sent = changed_sent + ' '
         else:
-            changed_sent = changed_sent + enc_convert(i, key1, key2)
-    return changed_sent
+            changed_sent = changed_sent + enc_convert(i, key1, key2)  # Encrypt each letter
+    return changed_sent  # Return encrypted text
 
-#returns decrypted values to affine_decrypt
+# Returns decrypted values to affine_decrypt
 def dec_convert(letter, key1, key2):
-    a = pow(key1,-1,26)
-    c = ((alpha_dict[letter] - key2) * a) % 26
-    m = alpha_list[c]
-    return m
+    a = pow(key1, -1, 26)  # Find modular multiplicative inverse
+    c = ((alpha_dict[letter] - key2) * a) % 26  # Decryption formula
+    m = alpha_list[c]  # Get decrypted letter
+    return m  # Return decrypted letter
 
-#Decrypts the given Cipher text by sending each letter to the dec_convert function
+# Decrypts the given Cipher text by sending each letter to the dec_convert function
 def affine_decrypt():
-    key1, key2 = get_keys()
-    sent = input("What text do you want to decrypt?: ")
-    actual_sent = sent.upper()
-    changed_sent = ''
+    key1, key2 = get_keys()  # Get decryption keys
+    sent = input("What text do you want to decrypt?: ")  # Get text to decrypt
+    actual_sent = sent.upper()  # Convert text to uppercase
+    changed_sent = ''  # Initialize decrypted text
     for i in actual_sent:
         if i.isnumeric == True:
             changed_sent = changed_sent
-        elif i in spec_chara:
+        elif i not in alpha_list and i != " ":
             changed_sent = changed_sent
         elif i == ' ':
             changed_sent = changed_sent + ' '
         else:
-            changed_sent = changed_sent + dec_convert(i, key1, key2)
-    return changed_sent
+            changed_sent = changed_sent + dec_convert(i, key1, key2)  # Decrypt each letter
+    return changed_sent  # Return decrypted text
 
-#Main function
+# Main function
 def main():
-    a = enc_or_dec()
+    a = enc_or_dec()  # Get user's choice (encrypt or decrypt)
     if a == '1':
-        value = affine_encrypt()
-        print(value)
+        value = affine_encrypt()  # Encrypt if user chooses 1
+        print(value)  # Print encrypted text
     elif a == '2':
-        value = affine_decrypt()
-        print(value)
+        value = affine_decrypt()  # Decrypt if user chooses 2
+        print(value)  # Print decrypted text
     elif a == 'q':
-        print("quitting!")
-        exit
-        
-#prevents the main function from being called when this python file is called somewhere else
+        print("quitting!")  # Quit if user chooses q
+        exit  # Exit the program
+
+# Prevents the main function from being called when this python file is called somewhere else
 if __name__ == "__main__":
     main()
